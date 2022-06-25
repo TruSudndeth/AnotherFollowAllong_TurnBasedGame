e3e69e387fbe6f0b75b66991b6e83afcddc5de43
TruSudndeth <72487248+TruSudndeth@users.noreply.github.com>
2022-06-24 23:05:30 -0700

    MyBranch"Unit Move"
    Challenge Complete: make the unit Stop when Close enough.

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -5,23 +5,28 @@ using UnityEngine;
 public class Unit : MonoBehaviour
 {
     [SerializeField]
-    private Vector3 targetPosition;
+    private Vector3 SetTarget;
+    private Vector3 targetPosition = Vector3.zero; 
     [SerializeField]
     private float moveSpeed = 1;
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetTarget = new Vector3(4,0,4);
     }
     // Update is called once per frame
     void Update()
     {
-        Vector3 moveDirection = (targetPosition - transform.position).normalized;
-        transform.position += moveDirection * Time.deltaTime * moveSpeed;
+        if((targetPosition - transform.position).sqrMagnitude > 1f)
+        {
+            Debug.Log("this code Stops");  //delete code
+            var moveDirection = targetPosition - transform.position;
+            transform.position += moveDirection.normalized * Time.deltaTime * moveSpeed; // moveSpeed will overshoot the distance 1f in if condition
+        }
 
         if(Input.GetKeyDown(KeyCode.T))
         {
-            Move(new Vector3(4,0,4));
+            Move(SetTarget);
         }
     }
     private void Move(Vector3 _targetPosition)
