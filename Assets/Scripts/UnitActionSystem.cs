d6c0c4ba202d195c070a54997bc65ee84e161f37
TruSudndeth <72487248+TruSudndeth@users.noreply.github.com>
2022-06-26 21:10:01 -0700

    Refactored code and more in Description
    Refactored UnitActionSystem to be more legible/clear also made UnitActionSystem Awake Run First thing before any other script.
    Unity>Edit>ProjectSetting>Script Execution Order> just Above Default.

--- Assets/Scripts/UnitActionSystem.cs
+++ Assets/Scripts/UnitActionSystem.cs
@@ -1,24 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class UnitActionSystem : MonoBehaviour
 {
     [SerializeField] private Unit selectedUnit;
+    private Vector3 Position;
+    private Collider Selection;
 
     private void Update()
     {
         if (Input.GetMouseButtonDown(MouseWorld.MInput.primary))
         {
-            Vector3 Position;
-            Collider Selection;
             MouseWorld.GetPosition(out Position, out Selection);
+            
             if(Selection.CompareTag("MousePlane"))
                 selectedUnit.Move(Position);
             if (Selection.CompareTag("Unit"))
-                selectedUnit = Selection.GetComponent<Unit>();
+                TrySwitchUnit();
         }
-        
+
     }
+        private void TrySwitchUnit()
+        {
+            //selectedUnit = Selection.GetComponent<Unit>(); // no null protection use TryGetComponent
+            if (Selection.TryGetComponent<Unit>(out Unit NewUnit)) 
+                selectedUnit = NewUnit;
+        }
 
 }
