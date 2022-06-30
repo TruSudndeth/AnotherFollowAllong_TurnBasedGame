884295a2545247e86ed434e7e504cde777a1fc01
TruSudndeth <72487248+TruSudndeth@users.noreply.github.com>
2022-06-30 01:14:08 -0700

    Cleaner Challenge Vector3.Lerp
    Challenge "Camera Zoom" (Smooth)
    used the square root difference to get the interpolation as was shown in the Unit Move Script.  and didn't use a Vector3.Lerp in my Base script as done in the tutorial
    Meeeeeeeeeh.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -12,8 +12,17 @@ public class CameraController : MonoBehaviour
     [SerializeField] private float moveSpeed = 10.0f;
     [SerializeField] private float rotateSpeed = 100.0f;
     [SerializeField] private float zoomAmount = 1.0f;
-    [SerializeField] private float smoothZoomSpeed = 1.0f;
+    [SerializeField] private float smoothZoomSpeed = 5.0f;
     [SerializeField] private bool inverted = false;
+
+    private CinemachineTransposer CMTransposer; // cached $$$
+    private Vector3 targetFollowOffset;
+
+    private void Start()
+    {
+        CMTransposer = CMVirtualCamera.GetCinemachineComponent<CinemachineTransposer>();
+        targetFollowOffset = CMTransposer.m_FollowOffset;
+    }
     // Update is called once per frame
     void Update()
     {
@@ -51,26 +60,20 @@ public class CameraController : MonoBehaviour
 
         transform.eulerAngles += rotateVector * rotateSpeed * Time.deltaTime;
 
-        CinemachineTransposer CMTransposer = CMVirtualCamera.GetCinemachineComponent<CinemachineTransposer>();
+        
 
-        Vector3 followOffset = CMTransposer.m_FollowOffset;
+        targetFollowOffset = CMTransposer.m_FollowOffset;
 
         if(Input.mouseScrollDelta.y > 0)
         {
-            followOffset.y -= zoomAmount * (inverted ? -1 : 1);
+            targetFollowOffset.y -= zoomAmount * (inverted ? -1 : 1);
         }
         if(Input.mouseScrollDelta.y < 0)
         {
-            followOffset.y += zoomAmount * (inverted ? -1 : 1);
+            targetFollowOffset.y += zoomAmount * (inverted ? -1 : 1);
         }
 
-        followOffset.y = Mathf.Clamp(followOffset.y, MIN_FOLLOW_Y_OFFSET, MAX_FOLLOW_Y_OFFSET);
-        Vector3 smoothZoom = followOffset;
-        if(Mathf.Sqrt(followOffset.y - CMTransposer.m_FollowOffset.y) > 0.10f)
-        {
-            smoothZoom += followOffset * Time.deltaTime * smoothZoomSpeed;
-        }
-        
-        CMTransposer.m_FollowOffset = smoothZoom;
+        targetFollowOffset.y = Mathf.Clamp(targetFollowOffset.y, MIN_FOLLOW_Y_OFFSET, MAX_FOLLOW_Y_OFFSET);
+        CMTransposer.m_FollowOffset = Vector3.Lerp(CMTransposer.m_FollowOffset, targetFollowOffset, Time.deltaTime * smoothZoomSpeed);
     }
 }
